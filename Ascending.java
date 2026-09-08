2e71bd489b4452190b03386ff9cca9b5172e2da8
Akshata Udagi <akshata.udagi@gmail.com>
2026-09-08 23:19:41 +0530

    Ascending

---
+++ Ascending.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Ascending {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int num[] = new int[size];
+
+        for(int i=0; i<size; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        boolean isAscending= true;
+
+        for(int i = 0; i<num.length-1; i++) {
+            if(num[i]>num[i+1]) {
+                isAscending = false;
+                break;
+            }
+            
+        }
+
+        if(isAscending) {
+            System.out.println("Array is in ascending order");
+        }
+        else {
+            System.out.println("Array is not in ascending order");
+        }
+
+    }
+}
