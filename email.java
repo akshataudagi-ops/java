7165ffd57518994019e0d521e544ff43772780c7
Akshata Udagi <akshata.udagi@gmail.com>
2026-09-14 01:13:03 +0530

    email

---
+++ email.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class email {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter email: ");
+        String email = sc.next();
+
+        String username = "";
+
+        for(int i = 0; i < email.length();i++){
+            if(email.charAt(i)=='@'){
+                break;
+            }
+            else{
+                username +=email.charAt(i);
+            }
+        }
+
+        System.out.println("Username: " + username);
+
+    }
+}
